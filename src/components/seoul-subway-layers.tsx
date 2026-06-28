a42295dc4ef2e6f0f8c8df6a05009bca202ce8f3
Geuni <dlrmsgnl620@naver.com>
2026-06-28 18:52:30 +0900

    feat(map): show subway station labels

--- src/components/seoul-subway-layers.tsx
+++ src/components/seoul-subway-layers.tsx
@@ -29,6 +29,27 @@ export function SeoulSubwayLayers({ stations }: SeoulSubwayLayersProps) {
           }}
         />
       ))}
+
+      <Layer
+        id="subway-station-label"
+        type="symbol"
+        minzoom={12.5}
+        layout={{
+          "text-field": ["get", "name"],
+          "text-font": ["Noto Sans Regular"],
+          "text-size": ["interpolate", ["linear"], ["zoom"], 12, 10, 15, 13],
+          "text-offset": [0.8, -0.8],
+          "text-anchor": "left",
+          "text-allow-overlap": false,
+          "text-ignore-placement": false,
+        }}
+        paint={{
+          "text-color": "#d8efe8",
+          "text-halo-color": "#020403",
+          "text-halo-width": 1.2,
+          "text-opacity": ["interpolate", ["linear"], ["zoom"], 12, 0, 13, 1],
+        }}
+      />
     </Source>
   );
 }
