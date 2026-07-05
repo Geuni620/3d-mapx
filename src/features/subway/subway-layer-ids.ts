bc1c0a207e30dfa527fc55ca36d06e6b7196f93e
Geuni <dlrmsgnl620@naver.com>
2026-07-05 22:59:31 +0900

    WIP: circle

--- src/features/subway/subway-layer-ids.ts
+++ src/features/subway/subway-layer-ids.ts
@@ -1,1 +1,10 @@
+export const FIRST_SUBWAY_STATION_CIRCLE_LAYER_ID = "subway-station-1";
+export const SUBWAY_STATION_CIRCLE_LAYER_ID_PREFIX = "subway-station";
+export const SUBWAY_STATION_CIRCLE_BORDER_LAYER_ID_PREFIX =
+  "subway-station-border";
+export const SUBWAY_STATION_CIRCLE_CORE_LAYER_ID_PREFIX =
+  "subway-station-core";
+export const SUBWAY_STATION_CIRCLE_GAP_LAYER_ID_PREFIX = "subway-station-gap";
+export const SUBWAY_STATION_CIRCLE_HIGHLIGHT_LAYER_ID_PREFIX =
+  "subway-station-highlight";
 export const SUBWAY_STATION_LABEL_LAYER_ID = "subway-station-label";
